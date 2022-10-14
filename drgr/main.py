071bc8e81b182247053cbd955d04ac41403d5392
Yassir <Morphe@users.noreply.github.com>
2022-10-14 19:38:04 -0400

    wandb train log

--- drgr/main.py
+++ drgr/main.py
@@ -9,7 +9,7 @@ from data import DataLoader
 from env import Env
 from eval import Evaluator
 from utils import OUNoise
-
+import wandb
 
 def train(config: Config, env: Env, agent: DDPGAgent, evaluator: Evaluator,
           df_eval_user: pd.DataFrame(), df_eval_group: pd.DataFrame()):
@@ -25,28 +25,32 @@ def train(config: Config, env: Env, agent: DDPGAgent, evaluator: Evaluator,
     :return:
     """
     rewards = []
-    for episode in range(config.num_episodes):
-        state = env.reset()
-        agent.noise.reset()
-        episode_reward = 0
+    with wandb.init(project=config.project, entity= config.entity,job_type="train", name=config.name) as run:
+        for episode in range(config.num_episodes):
+            state = env.reset()
+            agent.noise.reset()
+            episode_reward = 0
 
-        for step in range(config.num_steps):
-            action = agent.get_action(state)
-            new_state, reward, _, _ = env.step(action)
-            agent.replay_memory.push((state, action, reward, new_state))
-            state = new_state
-            episode_reward += reward
+            for step in range(config.num_steps):
+                action = agent.get_action(state)
+                new_state, reward, _, _ = env.step(action)
+                agent.replay_memory.push((state, action, reward, new_state))
+                state = new_state
+                episode_reward += reward
 
-            if len(agent.replay_memory) >= config.batch_size:
-                agent.update()
+                if len(agent.replay_memory) >= config.batch_size:
+                    agent.update()
 
-        rewards.append(episode_reward / config.num_steps)
-        print('Episode = %d, average reward = %.4f' % (episode, episode_reward / config.num_steps))
-        if (episode + 1) % config.eval_per_iter == 0:
-            for top_K in config.top_K_list:
-                evaluator.evaluate(agent=agent, df_eval=df_eval_user, mode='user', top_K=top_K)
-            for top_K in config.top_K_list:
-                evaluator.evaluate(agent=agent, df_eval=df_eval_group, mode='group', top_K=top_K)
+            rewards.append(episode_reward / config.num_steps)
+            print('Episode = %d, average reward = %.4f' % (episode, episode_reward / config.num_steps))
+
+            #LOG INTO WANDB
+            wandb.log({"episodes":episode, "average reward": (episode_reward / config.num_steps)})
+            if (episode + 1) % config.eval_per_iter == 0:
+                for top_K in config.top_K_list:
+                    evaluator.evaluate(agent=agent, df_eval=df_eval_user, mode='user', top_K=top_K)
+                for top_K in config.top_K_list:
+                    evaluator.evaluate(agent=agent, df_eval=df_eval_group, mode='group', top_K=top_K)
 
 
 if __name__ == '__main__':
