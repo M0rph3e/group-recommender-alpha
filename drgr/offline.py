11de48ca0a205365ce3f0e486e43df6c517bdbfb
Yassir <47499948+M0rph3e@users.noreply.github.com>
2022-12-23 20:20:28 -0500

    complete recoding for real offline training

--- drgr/offline.py
+++ drgr/offline.py
@@ -2,28 +2,43 @@ import numpy as np
 import pandas as pd
 import torch 
 
+import os
 from config import Config
 from agent import DDPGAgent
+from eval import Evaluator
 from env import Env
+from collections import deque
 import random
+import pickle
+import wandb
 
 class Offline(object):
     """
     Offline data generation
     """
     def __init__(self, config : Config):
-        self.item_num = config.item_num
-        self.group_num= config.group_num
-        self.action_size = config.action_size
-        self.history_length = config.history_length
+        self.config = config
+        #set random seed for reproducibility
+        np.random.seed(config.seed)
+
+        #offline data directory 
+        if not os.path.exists(self.config.offline_path):
+            os.mkdir(self.config.offline_path)
 
     def random_state(self):
         """
         generate random [group,states] during offline rollouts
         :return state (list) : group + K-item or list of action depending on config.action_size
         """
-        state = [np.random.randint(1,self.group_num)] + (random.sample(range(1,self.item_num),self.history_length)) #+ to concat
+        state = [np.random.randint(1,self.config.group_num)] + (random.sample(range(1,self.config.item_num),self.config.history_length)) #+ to concat
         return state
+
+    def random_action(self):
+        """
+        generate random action (item) during offline rollouts
+        :return action (int) : item to recommend in a transition
+        """
+        return np.random.randint(1,self.config.item_num)
 
     def random_reward(self):
         """
@@ -32,31 +47,53 @@ class Offline(object):
         """
         return random.randint(0,1)
 
-    def off_step(self, action : int):
+    def get_offline_data(self, policy="random"):
+        """
+        generate offline transition data 
+        :param config: configuration file:
+        :param policy: in ["random","famous"] : policy to simulate historical data (only random actually)
+        :return offline data buffer: np array simaliting dequeu in format ([state, action, reward, new_state],...) 
+        """
+        offline_save_path = os.path.join(self.config.offline_path, policy + '_' + 
+                                            str(self.config.offline_data_size) + '.pkl')
+        print("Generating offline data with " + policy + " policy")
+        buffer = deque([])
+        for i in range(self.config.offline_data_size):
+            if policy == 'random' :
+                buffer.append(self.random_policy())
+            elif policy=='famous':
+                pass #TO DO
+
+        #dump genrated data in pkl
+        with open(offline_save_path,'wb') as file:
+            pickle.dump(buffer,file)
+
+        return buffer
+
+
+
+    def random_policy(self):
         """
         offline policy during one training step
         :params: action (int) : action of the agent 
-        :return: new_state, reward, done, info
+        :return: state, action reward, new_state
         """
         state = self.random_state()
         group_id = state[0]
         history = state[1:] 
-
+        action = self.random_action()
         reward = self.random_reward()
 
         if reward > 0:
             history = history[1:] + [action]
 
         new_state = [group_id] + history
-        self.state = new_state
-        done = False
-        info = {}
 
-        return new_state, reward, done, info
+        return state, action, reward, new_state
 
 
-    def train_offline(self, agent: DDPGAgent,config : Config,
-          df_eval_user: pd.DataFrame(), df_eval_group: pd.DataFrame()):
+    def train_offline(self, evaluator:Evaluator,agent: DDPGAgent,
+                      df_eval_user: pd.DataFrame(), df_eval_group: pd.DataFrame(),policy='random',reload=False):
         """
         Train the agent offline without interacting with the environment
         :param config : configuration file
@@ -66,25 +103,38 @@ class Offline(object):
         :param df_eval_group: group evaluation data
         :return agent : agent trained offline so it can be deployed in the environment during training
         """
-        rewards = []
-        for episode in range(config.offline_episodes):
-            state = self.random_state()
-            agent.noise.reset()
-            episode_reward = 0
+        print("Training offline")
+        offline_save_path = os.path.join(self.config.offline_path, policy + '_' + 
+                                            str(self.config.offline_data_size) + '.pkl')
+        with wandb.init(project=self.config.project, entity= self.config.entity,job_type="train", name=self.config.name+'_offline') as run:
+            #load historical data buffer     
+            if not os.path.exists(offline_save_path) or reload:
+                buffer = self.get_offline_data(policy=policy)
+            else:
+                with open(offline_save_path, 'rb') as file:
+                    buffer = pickle.load(file)
 
-            for step in range(config.num_steps):
-                action = agent.get_action(state,with_noise=True)
-                new_state, reward, _, _ = self.off_step(action)
-                agent.replay_memory.push((state, action, reward, new_state)) #add to buffer
-                state = new_state
-                episode_reward += reward
-
-                if len(agent.replay_memory) >= config.batch_size:
+            for step in range(self.config.offline_step):
+                #put historical data to agent 
+                agent.replay_memory.push(buffer.pop())
+                if len(buffer)==0:
+                    print("UNROLLED ALL OFFLINE DATA")
+                    break
+                #update the agent on historical buffer data
+                if len(agent.replay_memory) >= self.config.batch_size:
                     agent.update()
 
-            rewards.append(episode_reward / config.num_steps)
-            print('Offine Episode = %d, average reward = %.4f' % (episode, episode_reward / config.num_steps))
+                #Evaluate agent each `offline_eval_per_step` 
+                if (step + 1) % self.config.offline_eval_per_step == 0:
+                    for top_K in self.config.top_K_list:
+                        avg_recall_score_user,avg_ndcg_score_user = evaluator.evaluate(agent=agent, df_eval=df_eval_user, mode='user', top_K=top_K)
+                        #log to WANDB
+                        wandb.log({"Average Recall@"+str(top_K)+" Score for user":avg_recall_score_user, "average NDCG@"+str(top_K)+" Score for user": avg_ndcg_score_user},step=step)
+
+                    for top_K in self.config.top_K_list:
+                        avg_recall_score_goup, avg_ndcg_score_group = evaluator.evaluate(agent=agent, df_eval=df_eval_group, mode='group', top_K=top_K)
+                        #log to WANDB
+                        wandb.log({"Average Recall@"+str(top_K)+" Score for Group":avg_recall_score_goup, "average NDCG@"+str(top_K)+" Score for Group": avg_ndcg_score_group},step=step)
+
             
-            #clear buffer before pytting online
-            agent.replay_memory.buffer.clear()
         return agent
